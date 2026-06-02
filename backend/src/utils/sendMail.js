dde648dff3e863c54bce3bcd99a22ba7e8c8e8f9
Shivam Rai <raishivamrai837@gmail.com>
2026-06-03 00:03:04 +0530

    Fix Gmail SMTP timeout

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -1,15 +1,21 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
-const sendMail = async (to, subject,html) => {
-
+const sendMail = async (to, subject, html) => {
   const info = await transporter.sendMail({
     from: `MyCart <${process.env.EMAIL_USER}>`,
     to,
@@ -18,7 +24,6 @@ const sendMail = async (to, subject,html) => {
   });
 
   console.log("Email sent:", info.messageId);
-
   return info;
 };
 
