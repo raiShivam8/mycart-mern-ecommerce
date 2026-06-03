f4acd9ebc57b1af0f748d948ad4891fdc10c6ccf
Shivam Rai <raishivamrai837@gmail.com>
2026-06-03 12:29:08 +0530

    Update Brevo SMTP mail config

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -1,17 +1,27 @@
 const nodemailer = require("nodemailer");
 
+console.log("SMTP HOST:", process.env.SMTP_HOST);
+console.log("SMTP PORT:", process.env.SMTP_PORT);
+console.log("SMTP USER:", process.env.SMTP_USER);
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: Number(process.env.SMTP_PORT || 587),
   secure: false,
+  requireTLS: true,
 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+
+  connectionTimeout: 60000,
+  greetingTimeout: 60000,
+  socketTimeout: 60000,
 });
 
 const sendMail = async (to, subject, html) => {
+  console.log("Sending email to:", to);
 
   const info = await transporter.sendMail({
     from: `MyCart <${process.env.SMTP_USER}>`,
